c774dc318890a7d6d8d522c84fa5dbfe5a5e6fc5
Alain Guillon <zyrass@outlook.fr>
2021-09-24 16:57:54 +0200

    Add a save texte on json file

--- index.php
+++ index.php
@@ -5,6 +5,16 @@ const ERROR_REQUIRED = "Désolé, ce champ ne peut-être vide.";
 const ERROR_TOO_SHORT = "Désolé, vous avez saisie moins de 5 caractères.";
 $error = "";
 
+// Création du fichier contenant nos todos. (Format JSON)
+$filename = __DIR__ . '/data/todos.json';
+$todos = [];
+
+// Check si le fichier existe sinon on définit un tableau vide
+if (file_exists($filename)) {
+  $data = file_get_contents($filename);
+  $todos = json_decode($data, true); // Convertie en tableau Associatif
+}
+
 // Contrôle qu'on a bien une méthode POST
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
@@ -17,6 +27,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $error = ERROR_REQUIRED;
   } elseif (mb_strlen($todo) < 5) {
     $error = ERROR_TOO_SHORT;
+  };
+
+  // Si je n'ai pas d'erreur j'ajoute le contenu de la nouvelle todo
+  if (!$error) {
+    $todos = [...$todos, [
+      "id" => time(),
+      "name" => $todo,
+      "done" => false
+    ]];
+
+    file_put_contents($filename, json_encode($todos), JSON_PRETTY_PRINT);
   }
 }
 
